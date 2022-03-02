c6cd2f989b350d8aed30dc986ac2a9b9bf37d76d
NateStrings <98417856+NateStrings@users.noreply.github.com>
2022-03-02 00:15:41 -0500

    Note update

--- countdown timer.py
+++ countdown timer.py
@@ -2,8 +2,7 @@ import time
 
 s = int(input("Seconds: ")) # Get users input as an integer
 
-for i in range(s):
-# you can also replace above line with "while  s > 0 :""
+for i in range(s): # you can also replace above line with "while  s > 0 :""
     print(str(s), end = "\r")
     s -= 1 #this is essentially s = s - 1
     time.sleep(1) # sleep for one second between loops
